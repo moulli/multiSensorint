d179225f82f75aeabf55ce819388ff14fad914f4
Moulli <hippolyte.moulle@upmc.fr>
2019-05-17 17:47:06 +0200

    Worked on first plot option

--- visualisation_app/app02_regressors/automaticPlot1.m
+++ visualisation_app/app02_regressors/automaticPlot1.m
@@ -3,9 +3,8 @@ function automaticPlot1(app)
 
 
     % Recovers values:
-    obj1 = app.set1;
-    obj2 = app.set2;
-    bestneurons = [app.nneu1, app.nneu2];
+    obj1 = app.zset1;
+    obj2 = app.zset2;
     % Checking grids:
     if ~isequal(obj1.gridsize(1:3), obj2.gridsize(1:3))
         error('Objects do not have same gridsize.')
@@ -13,97 +12,59 @@ function automaticPlot1(app)
         sgrid = obj1.gridsize(1:3);
     end
     
+    % Adapt regressors in dataset 1:
+    otemp1 = flatten(obj1);
+    zregress1 = otemp1.Zcorrel;
+    zregress1 = log10(zregress1 - min(zregress1) + 1);
+    zregress1 = zregress1 / max(zregress1);
     % What to keep in dataset 1:
-    otemp1 = flatten(obj1);
-    tokeep1 = (otemp1.Zcorrel >= app.maval1);
+    [~, tokeep1] = sort(zregress1, 'descend');
+    tokeep1 = tokeep1(1:app.nneu1);
+    % Simplifying index and correlation:
     zindex1 = otemp1.Zindex(tokeep1);
-    zcorrel1 = otemp1.Zcorrel(tokeep1);
-    % Building grid for dataset 1:
-    grid1 = zeros(sgrid);
-    grid1(zindex1) = zcorrel1;
+    zcorrel1 = zregress1(tokeep1);
+    % Filling grid:
+    grid_color_1 = zeros(obj1.gridsize(1:3));
+    grid_color_1(zindex1) = zcorrel1;
     
+    % Adapt regressors in dataset 2:
+    otemp2 = flatten(obj2);
+    zregress2 = otemp2.Zcorrel;
+    zregress2 = log10(zregress2 - min(zregress2) + 1);
+    zregress2 = zregress2 / max(zregress2);
     % What to keep in dataset 2:
-    otemp2 = flatten(obj2);
-    tokeep2 = (otemp2.Zcorrel >= app.maval2);
+    [~, tokeep2] = sort(zregress2, 'descend');
+    tokeep2 = tokeep2(1:app.nneu2);
+    % Simplifying index and correlation:
     zindex2 = otemp2.Zindex(tokeep2);
-    zcorrel2 = otemp2.Zcorrel(tokeep2);
-    % Building grid for dataset 2:
-    grid2 = zeros(sgrid);
-    grid2(zindex2) = zcorrel2;
-    %%%%%%%%%%%%%%%%%PAUSED HERE
+    zcorrel2 = zregress2(tokeep2);
+    % Filling grid:
+    grid_color_2 = zeros(obj2.gridsize(1:3));
+    grid_color_2(zindex2) = zcorrel2;
     
-    
-
-    % Binarization:
-    otemp1 = flatten(obj1);
-    grid1 = zeros(sgrid);
-    [~, ind_correl1] = sort(otemp1.Zcorrel, 'descend');
-    ind_correl1 = ind_correl1(1:bestneurons(1));
-    grid1(otemp1.Zindex(ind_correl1)) = 1;
-    otemp2 = flatten(obj2);
-    grid2 = zeros(sgrid);
-    [~, ind_correl2] = sort(otemp2.Zcorrel, 'descend');
-    ind_correl2 = ind_correl2(1:bestneurons(2));
-    grid2(otemp2.Zindex(ind_correl2)) = 2;
-    % Summing:
-    gridt = grid1 + grid2; find(gridt ~= 0);
-
-    % Defining points:
-    pt_both = find(gridt == 3);
-    pt_o1 = find(gridt == 1);
-    pt_o2 = find(gridt == 2);
-    % Defining plots:
-    [x_both, y_both, z_both] = ind2sub(sgrid, pt_both);
-    [x_o1, y_o1, z_o1] = ind2sub(sgrid, pt_o1);
-    [x_o2, y_o2, z_o2] = ind2sub(sgrid, pt_o2);
-
+    % What points to plot in the grid:
+    pts_plot = find((grid_color_1 + grid_color_2) ~= 0);
+    [pts_x, pts_y, pts_z] = ind2sub(obj1.gridsize, pts_plot);
     % Using right coordinates:
     xtemp = (obj1.xgrid(2:end)' + obj1.xgrid(1:end-1)') / 2;
     ytemp = (obj1.ygrid(2:end)' + obj1.ygrid(1:end-1)') / 2;
     ztemp = (obj1.zgrid(2:end)' + obj1.zgrid(1:end-1)') / 2;
-    compval = @(X) [xtemp(X(:, 1)), ytemp(X(:, 2)), ztemp(X(:, 3))];
-    t_both_temp = compval([x_both, y_both, z_both]);
-    x_both = t_both_temp(:, 1);
-    y_both = t_both_temp(:, 2);
-    z_both = t_both_temp(:, 3);
-    t_o1_temp = compval([x_o1, y_o1, z_o1]);
-    x_o1 = t_o1_temp(:, 1);
-    y_o1 = t_o1_temp(:, 2);
-    z_o1 = t_o1_temp(:, 3);
-    t_o2_temp = compval([x_o2, y_o2, z_o2]);
-    x_o2 = t_o2_temp(:, 1);
-    y_o2 = t_o2_temp(:, 2);
-    z_o2 = t_o2_temp(:, 3);
-
+    pts_x = xtemp(pts_x);
+    pts_y = ytemp(pts_y);
+    pts_z = ztemp(pts_z);
+    % Colors:
+%     pts_col = [grid_color_1(pts_plot), zeros(length(pts_plot), 1), grid_color_2(pts_plot)];
+%     pts_col = [1+(1-grid_color_2(pts_plot)), (1-grid_color_1(pts_plot))+(1-grid_color_2(pts_plot)), 1+(1-grid_color_1(pts_plot))] / 2;
+%     pts_col = [grid_color_1(pts_plot), 1-max([grid_color_1(pts_plot), grid_color_2(pts_plot)], [], 2), grid_color_2(pts_plot)];
+    gcol1 = grid_color_1(pts_plot);
+    gcol2 = grid_color_2(pts_plot);
+    col_first = 1.*(gcol1 > 0 & gcol2 == 0) + (1-gcol2).*(gcol1 == 0 & gcol2 > 0) + 1.*(gcol1 > 0 & gcol2 > 0);
+    col_second = (1-gcol1).*(gcol1 > 0 & gcol2 == 0) + (1-gcol2).*(gcol1 == 0 & gcol2 > 0) + (1-(gcol1+gcol2)/2).*(gcol1 > 0 & gcol2 > 0);
+    col_third = (1-gcol1).*(gcol1 > 0 & gcol2 == 0) + 1.*(gcol1 == 0 & gcol2 > 0) + 1.*(gcol1 > 0 & gcol2 > 0);
+    pts_col = [col_first, col_second, col_third];
+    
     % Plotting:
-    scatter3(app.UIAxes, x_both, y_both, z_both, app.mrksize1, [0, 0, 0], 'filled');
-    hold(app.UIAxes, 'on')
-    scatter3(app.UIAxes, x_o1, y_o1, z_o1, app.mrksize2, [0, 1, 0], 'filled')
-    scatter3(app.UIAxes, x_o2, y_o2, z_o2, app.mrksize2, [1, 0, 1], 'filled')
-        % Plotting selected points:
-        scatter3(app.UIAxes, app.selpoint1(1), app.selpoint1(2), app.selpoint1(3), 50, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', [0, 1, 0])
-        scatter3(app.UIAxes, app.selpoint2(1), app.selpoint2(2), app.selpoint2(3), 50, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', [1, 0, 1])
-        scatter3(app.UIAxes, app.mypoint(1), app.mypoint(2), app.mypoint(3), 50, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', [1, 0, 0])
-    app.UIAxes.DataAspectRatio = [1, 1, 1];
-    app.UIAxes.XGrid = 'on';
-    app.UIAxes.YGrid = 'on';
-    app.UIAxes.ZGrid = 'on';
-    title(app.UIAxes, 'Binary comparison between two objects: common in black, green for 1st, purple for 2nd')
-    xlabel(app.UIAxes, 'x-axis', 'Interpreter', 'latex')
-    ylabel(app.UIAxes, 'y-axis', 'Interpreter', 'latex')
-    zlabel(app.UIAxes, 'z-axis', 'Interpreter', 'latex')
-    view(app.UIAxes, app.aview)
-    hold(app.UIAxes, 'off')
-    if app.arotate == 1
-        rotate3d(app.UIAxes,'on')
-        zoom(app.UIAxes,'off')
-    elseif app.azoom == 1
-        rotate3d(app.UIAxes,'off')
-        zoom(app.UIAxes,'on')
-    else
-        rotate3d(app.UIAxes,'off')
-        zoom(app.UIAxes,'off')
-    end 
+    scatter3(app.UIAxes, pts_x, pts_y, pts_z, app.mrksize, pts_col, 'filled')
     
     
 end
