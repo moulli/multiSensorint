5479119681529fd7637faf2d7b8f4d2bca52d2fb
Moulli <hippolyte.moulle@upmc.fr>
2019-07-09 21:31:46 +0200

    Finished thresholding for F-statistic

--- a12_threshold_Fstat.m
+++ a12_threshold_Fstat.m
@@ -164,31 +164,31 @@ regressors = [ones(length(stim), 1), abs(expconv(stim'.*(stim'>0))), abs(expconv
 for i = 2:size(regressors, 2)
     regressors(:, i) = (regressors(:, i)-mean(regressors(:, i))) ./ std(regressors(:, i));
 end
-% Computing 
-dirpath = '/home/ljp/Science/Hippolyte/ALL_DATASETS';
-dirdata = dir(dirpath);
-for i = 1:length(dirdata)
-    % Informations on file:
-    ntemp = dirdata(i).name; % name of the file
-    disp(ntemp)
-    ptemp = fullfile(dirpath, ntemp); % name of path to file
-    % If non spontaneous activity then proceding:
-    if startsWith(ntemp, '20') && isempty(regexp(ntemp, 'spontaneous', 'once')) 
-        try
-        catch
-        end
-    end
-end
-
-
-fo = fitoptions('Method', 'NonlinearLeastSquares', 'Lower', [0.001, -Inf, 0.001], 'Upper', [Inf, Inf, Inf]);
-percentage = zeros(1, length(zbg));
-for i = 1:length(zbg)
-    [b, a] = hist(zbg(i).Zcorrel, 1000);
-    f = fit(a.',b.','gauss1', fo);
-    temp = zbg(i).Zcorrel >= f.b1 + 2*f.c1^2;
-    percentage(i) = sum(temp)/length(temp);
-end
+% % Computing 
+% dirpath = '/home/ljp/Science/Hippolyte/ALL_DATASETS';
+% dirdata = dir(dirpath);
+% for i = 1:length(dirdata)
+%     % Informations on file:
+%     ntemp = dirdata(i).name; % name of the file
+%     disp(ntemp)
+%     ptemp = fullfile(dirpath, ntemp); % name of path to file
+%     % If non spontaneous activity then proceding:
+%     if startsWith(ntemp, '20') && isempty(regexp(ntemp, 'spontaneous', 'once')) 
+%         try
+%         catch
+%         end
+%     end
+% end
+% 
+% 
+% fo = fitoptions('Method', 'NonlinearLeastSquares', 'Lower', [0.001, -Inf, 0.001], 'Upper', [Inf, Inf, Inf]);
+% percentage = zeros(1, length(zbg));
+% for i = 1:length(zbg)
+%     [b, a] = hist(zbg(i).Zcorrel, 1000);
+%     f = fit(a.',b.','gauss1', fo);
+%     temp = zbg(i).Zcorrel >= f.b1 + 2*f.c1^2;
+%     percentage(i) = sum(temp)/length(temp);
+% end
 
 % No need to fit gaussians, as it is an F-distribution...
 
@@ -234,40 +234,41 @@ Fmin1 = stat(2);
 fprintf('Minimum F-statistic for averaged signal method is %.2f, that is %.0f neurons \n', [Fmin1, sum(stats(:, 2)>Fmin1)]);
 
 % 2nd analysis: same for averaged sample of signal:
-numsample = size(dff, 1);
-numsignals = 2;
-avsample = zeros(numsample, size(dff, 2));
-for i = 1:numsample
+numsamples = 1000;
+numsignals = 20;
+avsample = zeros(numsamples, size(dff, 2));
+for i = 1:numsamples
     sample = randperm(size(dff, 1), numsignals);
     avsample(i, :) = mean(dff(sample, :));
 end
 warning('off')
 fprintf('Launching multilinear regression for all neurons. \n');
-avcoeffs = zeros(numsample, size(regressors, 2));
-avresiduals = zeros(numsample, length(stim));
-avstats = zeros(numsample, 4);
-for i = 1:numsample
+avcoeffs = zeros(numsamples, size(regressors, 2));
+avresiduals = zeros(numsamples, length(stim));
+avstats = zeros(numsamples, 4);
+for i = 1:numsamples
     [avcoeff, ~, avresidual, ~, avstat] = regress(avsample(i, :)', regressors);
     avcoeffs(i, :) = avcoeff';
     avresiduals(i, :) = avresidual';
     avstats(i, :) = avstat;
-    showProgress(i, numsample);
+    showProgress(i, numsamples);
 end
 fprintf('\n');
 warning('on')
-Fmin2 = max(avstats(:, 2));
+avstatsF = avstats(:, 2);
+Fmin2 = max(avstatsF);
 fprintf('Minimum F-statistic for averaged sample method is %.2f, that is %.0f neurons \n', [Fmin2, sum(stats(:, 2)>Fmin2)]);
 % Fitting gaussian:
 % f = fit(a', b', 'gauss1');
 % Fmin3 = f.b1+3*f.c1;
-pd_norm = fitdist(avstats(:, 2), 'Normal');
-pd_plot = normpdf(0:a(end), pd_norm.mu, pd_norm.sigma);
+pd_norm = fitdist(avstatsF, 'Normal');
 Fmin3 = pd_norm.mu + 3*pd_norm.sigma;
 fprintf('Minimum F-statistic at 3 std for averaged sample method is %.2f, that is %.0f neurons \n', [Fmin3, sum(stats(:, 2)>Fmin3)]);
 Fmin4 = pd_norm.mu + 5*pd_norm.sigma;
 fprintf('Minimum F-statistic at 5 std for averaged sample method is %.2f, that is %.0f neurons \n', [Fmin4, sum(stats(:, 2)>Fmin4)]);
 % Plotting histogram and gaussian:
-[b, a] = hist(avstats(:, 2), max([10, round(numsample/100)]));
+[b, a] = hist(avstatsF, max([10, round(numsamples/100)]));
+pd_plot = normpdf(0:a(end), pd_norm.mu, pd_norm.sigma);
 b = b / max(b) * max(pd_plot);
 figure; plot(a, b)
 hold on
@@ -277,9 +278,104 @@ plot([Fmin2, Fmin2], [0, max(pd_plot)], 'k')
 plot([Fmin3, Fmin3], [0, max(pd_plot)], 'k')
 plot([Fmin4, Fmin4], [0, max(pd_plot)], 'k')
 % Plotting Fstatistic
-[bf, af] = hist(stats(:, 2), max([10, round(numsample/100)]));
+[bf, af] = hist(stats(:, 2), max([10, round(numsamples/100)]));
 bf = bf / max(bf) * max(pd_plot);
 plot(a, bf, ':')
 
+% This analysis seems legit. How many signals should we average in this
+% randomized F-statistic analysis though?
 
+
+%% Analyzing number of signals to randomize:
+
+numsamples = 10000;
+numsignals = 1:10;
+lens = length(numsignals);
+meandist = zeros(numsamples, lens);
+Fstatana = zeros(numsamples, lens);
+warning('off')
+meandff = mean(dff);
+for j = 1:lens
+    avsampleFstatana = zeros(numsamples, size(dff, 2));
+    for i = 1:numsamples
+        sample = randperm(size(dff, 1), numsignals(j));
+        avsampleFstatana(i, :) = mean(dff(sample, :), 1);
+    end
+    meandist(:, j) = sqrt(sum((avsampleFstatana - meandff).^2, 2));
+    showProgress(j, lens);
+end
+
+
+%% Final algorithm for randomized analysis:
+
+
+% Determining number of signals used to randomize.
+% This algorithm is based on the standard deviation between randomized
+% signal of a few samples (RSFS), and randomized signal of all signals 
+% (RSAS). Defining a threshold value (std_div), we sample using more and 
+% more signals, and as soon as the standard deviation of the difference 
+% between RSFS and RSAS goes below std_set/std_div, we keep the number of
+% signals for later.
+
+% Parameters:
+numsamples = 1000;
+mean_dff = mean(dff, 1);
+std_set = mean(std(dff, [], 2));
+std_div = 4;
+
+% While loop:
+numsignal = 1;
+while true
+    av_signals = zeros(numsamples, size(dff, 2));
+    for i = 1:numsamples
+        sample = randperm(size(dff, 1), numsignal);
+        av_signals(i, :) = mean(dff(sample, :), 1);
+    end
+    std_temp = mean(std(av_signals-mean_dff), 2);
+    %disp([numsignal, std_temp, std_set/std_div])
+    if std_temp <= std_set/std_div
+        break
+    end
+    numsignal = numsignal + 1;
+end
+
+% Print result:
+fprintf('Number of signals to randomize will be %.0f \n', numsignal);
+
+
+% Determining threshold value for F-statistic:
+% We use averaged signals we computed just above, for randomized signals,
+% with numsignal signals. We do a multilinear regression on these fake
+% signals, and only keep F-statistic. Then we fit a gaussian on this
+% distribution. As these randomized signal are not supposed to be
+% significant, we want to take an F-statistic limit that will ensure actual
+% neurons are more significant than former distribution. We pick Flim equal
+% to the mean of the gaussian, to which we add 5 times the standard
+% deviation. 
+
+% Define regressors:
+regressors = [ones(length(stim), 1), abs(expconv(stim'.*(stim'>0))), abs(expconv(stim'.*(stim'<0))), ...
+              abs(expconv(gradient(stim').*(gradient(stim')>0))), abs(expconv(gradient(stim').*(gradient(stim')<0)))];
+for i = 2:size(regressors, 2)
+    regressors(:, i) = (regressors(:, i)-mean(regressors(:, i))) ./ std(regressors(:, i));
+end
+
+% Regress against fake signals:
+warning('off')
+av_Fstat = zeros(numsamples, 1);
+for i = 1:numsamples
+    [~, ~, ~, ~, av_stats] = regress(av_signals(i, :)', regressors);
+    av_Fstat(i) = av_stats(2);
+    showProgress(i, numsamples);
+end
+
+% Find normal distribution fitting these F-stats:
+pd_norm = fitdist(av_Fstat, 'Normal');
+Flim = pd_norm.mu + 5*pd_norm.sigma;
+
+% Print results:
+fprintf('Minimum F-statistic at 5 std for averaged sample method is %.2f \n', Flim);
+
+% Optional: number of neurons kept with this technique:
+fprintf('Number of neurons kept is %.0f \n', sum(stats(:, 2) > Flim));
 
